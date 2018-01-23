e313b57f455b231bfda50292b4ec5d5e692d25a6
Maestro <Maestro@imac-de-maestro-102.local>
2018-01-22 18:26:56 -0700

    Primer commit

--- springs/ViewController.swift
+++ springs/ViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var vwCuadro: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        //vwCuadro.alpha = 0.0
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        UIView.animate(withDuration: 2, delay: 1, usingSpringWithDamping: 0.4,
+                       initialSpringVelocity: 0.2, options: [], animations: {
+            self.vwCuadro.center.y += 200
+            //self.vwCuadro.alpha = 1.0
+            self.vwCuadro.backgroundColor = UIColor.white
+            self.vwCuadro.transform =
+                CGAffineTransform(scaleX: 1, y: 1)
+        }, completion: nil)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
